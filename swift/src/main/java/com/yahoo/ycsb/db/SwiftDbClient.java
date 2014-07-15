5fd32d0401fcf65b78bee2396985760faa1f85ab
Lorenzo Miori <memoryS60@gmail.com>
2014-07-15 11:55:27 +0200

    - lots of fixes to the port. - Tool is now working okay => still needs testing + cleanup

--- swift/src/main/java/com/yahoo/ycsb/db/SwiftDbClient.java
+++ swift/src/main/java/com/yahoo/ycsb/db/SwiftDbClient.java
@@ -11,14 +11,17 @@ package com.yahoo.ycsb.db;
 
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.lang.reflect.Type;
 import java.net.URI;
 import java.net.URL;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
-import java.util.Properties;
 import java.util.Set;
 import java.util.Vector;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -27,15 +30,17 @@ import ch.iterate.openstack.swift.AuthenticationResponse;
 import ch.iterate.openstack.swift.Client;
 import ch.iterate.openstack.swift.Client.AuthVersion;
 import ch.iterate.openstack.swift.io.ContentLengthInputStream;
+import ch.iterate.openstack.swift.model.Container;
 import ch.iterate.openstack.swift.model.Region;
 import ch.iterate.openstack.swift.model.StorageObject;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
-import com.yahoo.ycsb.ByteArrayByteIterator;
+import com.woorea.openstack.swift.Swift;
 import com.yahoo.ycsb.ByteIterator;
 import com.yahoo.ycsb.DB;
 import com.yahoo.ycsb.DBException;
+import com.yahoo.ycsb.StringByteIterator;
 
 /**
  * MongoDB client for YCSB framework.
@@ -64,6 +69,16 @@ public class SwiftDbClient extends DB {
 	 */
 	private static final AtomicInteger initCount = new AtomicInteger(0);
 
+	class SimpleDocument {
+		public String field;
+		public String value;
+
+		public SimpleDocument(String field, String value) {
+			this.field = field;
+			this.value = value;
+		}
+	}
+
 	/**
 	 * Initialize any state for this DB. Called once per DB instance; there is
 	 * one DB instance per client thread.
@@ -78,23 +93,15 @@ public class SwiftDbClient extends DB {
 			}
 
 			// initialize swift driver
-			// Properties props = getProperties();
-			// TODO set a localhost default value !!
-			// String url = props.getProperty("swift.url", "10.10.242.6:8080");
-			// database = props.getProperty("mongodb.database", "ycsb");
-			// String writeConcernType =
-			// props.getProperty("mongodb.writeConcern",
-			// "safe").toLowerCase();
-			// final String maxConnections = props.getProperty(
-			// "mongodb.maxconnections", "10");
-
 			try {
-				URI authUri = new URL("http", "10.10.242.6", 8080,
+				URI authUri = new URL("http", "10.10.243.6", 8080,
 						"/auth/v1.0/").toURI();
 				System.out.println("Attempting connection with " + authUri);
 				swiftdb = new Client(60 * 1000); // ms !!!!!!
+				// AuthVersion authVersion, URI authenticationURL, String
+				// username, String password, String tenantId
 				swiftAuthentication = swiftdb.authenticate(AuthVersion.v10,
-						authUri, "test:tester", "testing", "tester");
+						authUri, "system:root", "testpass", "root");
 				System.out.println("Swift connection created with " + authUri);
 				System.out.println("X-Auth-Token: "
 						+ swiftAuthentication.getAuthToken());
@@ -109,7 +116,6 @@ public class SwiftDbClient extends DB {
 				throw new DBException(
 						"Could not initialize Swift connection pool for Loader: "
 								+ e1.toString());
-				// return;
 			}
 		}
 	}
@@ -146,30 +152,20 @@ public class SwiftDbClient extends DB {
 	public int delete(String table, String key) {
 		try {
 			Region r = (Region) (swiftAuthentication.getRegions()).toArray()[0];
-			swiftdb.deleteObject(r, table, key);
+			for (StorageObject object : swiftdb.listObjects(r, key)) {
 
+				swiftdb.deleteObject(r, key, object.getName());
+
+			}
+			swiftdb.deleteContainer(r, key);
+			// swiftdb.deleteObject(r, key, key);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 			return 1;
+		} finally {
+			// eventually used
 		}
 		return 0;
-		// com.mongodb.DB db = null;
-		// try {
-		// db = mongo.getDB(database);
-		// db.requestStart();
-		// DBCollection collection = db.getCollection(table);
-		// DBObject q = new BasicDBObject().append("_id", key);
-		// WriteResult res = collection.remove(q, writeConcern);
-		// return res.getN() == 1 ? 0 : 1;
-		// } catch (Exception e) {
-		// System.err.println(e.toString());
-		// return 1;
-		// } finally {
-		// if (db != null) {
-		// db.requestDone();
-		// }
-		// }
 	}
 
 	/**
@@ -193,24 +189,62 @@ public class SwiftDbClient extends DB {
 		// 0 is the regionId, that actually matters only for .equal purpouses
 		try {
 			Region r = (Region) (swiftAuthentication.getRegions()).toArray()[0];
-			if (!swiftdb.containerExists(r, table)) {
-				swiftdb.createContainer(r, table);
-				System.out.println("We should create container: " + table
+			if (!swiftdb.containerExists(r, key)) {
+				swiftdb.createContainer(r, key);
+				System.out.println("We should create container: " + key
 						+ " in " + r);
 			}
-			Type typeOfMap = new TypeToken<Map<String, ByteIterator>>() {
-			}.getType();
 
-			Gson gson = new Gson();
-			String json = gson.toJson(values, typeOfMap);
+			HashMap<String, String> fields = StringByteIterator
+					.getStringMap(values);
+
+			System.out.println("INSERT " + table + " (:=region) " + key);
+
+			for (String fieldKey : fields.keySet()) {
+				InputStream data = new ByteArrayInputStream(values
+						.get(fieldKey).toString().getBytes());
+				System.out.println("\tField" + fieldKey + " len: "
+						+ values.get(fieldKey).bytesLeft());
+				// region, container, data, type, key
+				swiftdb.storeObject(r, key, data, "application/text", fieldKey,
+						new HashMap<String, String>());
+			}
+
+			// Type typeOfMap = new TypeToken<Map<String, String>>() {
+			// }.getType();
+			// String json = new Gson().toJson(fields, typeOfMap);
+			// System.out.println("INSERT " + table + " " + key + " object: "
+			// + json);
 
 			// region, container, data, type, key
-			swiftdb.storeObject(r, table, new ByteArrayInputStream(json
-					.toString().getBytes()), "application/json", key,
-					new HashMap<String, String>());
+			// swiftdb.storeObject(r, table,
+			// new ByteArrayInputStream(json.getBytes()),
+			// "application/json", key, new HashMap<String, String>());
+			//
+
+			// // Type typeOfMap = new TypeToken<Map<String, String>>() {
+			// // }.getType();
+			// // HashMap<String, String> content = new HashMap<String,
+			// String>();
+			//
+			// // values.put("_id", new StringByteIterator(key));
+			// // for (String k : values.keySet()) {
+			// // content.put(k, "" + values.get(k));
+			// // }
+			//
+			// // TODO actually the key should be saved as METADATA
+			// // but let's start with this at the moment
+			// // Gson gson = new Gson();
+			// // String json = gson.toJson(content, typeOfMap);
+			// // System.out.println("INSERT " + table + " " + key + " object: "
+			// // + json);
+			//
+			// // region, container, data, type, key
+			// // swiftdb.storeObject(r, table,
+			// // new ByteArrayInputStream(json.getBytes()),
+			// // "application/json", key, new HashMap<String, String>());
 
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 			return 1;
 		}
@@ -258,38 +292,44 @@ public class SwiftDbClient extends DB {
 			HashMap<String, ByteIterator> result) {
 		try {
 			Region r = (Region) (swiftAuthentication.getRegions()).toArray()[0];
-			// String json =
-			StringBuilder builder = new StringBuilder();
 
-			ContentLengthInputStream s = swiftdb.getObject(r, table, key);
-	        String json = "";
-	        while (true)
-	        {
-	            int x = -1;
-	            try
-	            {
-	                x = s.read();
-	            }
-	            catch (IOException e)
-	            {
-	                e.printStackTrace();
-	            }
-	            if (x != -1)
-	                json += String.valueOf((char) x);
-	            else
-	                break;
-	        }
+			Set<String> fieldsToRead;
 
-			final Gson gson = new Gson();
-			final String string = gson.toJson(json);
-			final Type type = new TypeToken<LinkedHashMap<String, ByteIterator>>(){}.getType();
-			final Map<String, ByteIterator> map2 = gson.fromJson(string, type);
-			result.putAll(map2);
+			if (fields == null) {
+				fieldsToRead = new HashSet<String>();
+				for (StorageObject object : swiftdb.listObjects(r, key)) {
+					fieldsToRead.add(object.getName());
+				}
+			} else {
+				fieldsToRead = fields;
+			}
+
+			for (String field : fieldsToRead) {
+				ContentLengthInputStream s = swiftdb.getObject(r, key, field);
+				String data = "";
+
+				while (true) {
+					int x = -1;
+					try {
+						x = s.read();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+					if (x != -1) {
+						data += String.valueOf((char) x);
+					} else
+						break;
+				}
+
+				result.put(field, new StringByteIterator(data));
+
+			}
 
 		} catch (IOException e) {
 			e.printStackTrace();
 			return 1;
 		}
+
 		return 0;
 		// com.mongodb.DB db = null;
 		// try {
@@ -343,7 +383,26 @@ public class SwiftDbClient extends DB {
 	@Override
 	public int update(String table, String key,
 			HashMap<String, ByteIterator> values) {
-		return 1;
+
+		HashMap<String, ByteIterator> current = new HashMap<String, ByteIterator>();
+
+		// read + parse
+
+		if (read(table, key, null, current) != 0) {
+			return 1;
+		}
+
+		// update
+		for (String k : values.keySet()) {
+			current.put(k, values.get(k));
+		}
+
+		// write back
+		if (insert(table, key, current) != 0) {
+			return 1;
+		}
+
+		return 0;
 		// com.mongodb.DB db = null;
 		// try {
 		// db = mongo.getDB(database);
@@ -395,7 +454,27 @@ public class SwiftDbClient extends DB {
 	@Override
 	public int scan(String table, String startkey, int recordcount,
 			Set<String> fields, Vector<HashMap<String, ByteIterator>> result) {
-		return 1;
+
+		try {
+			Region r = (Region) (swiftAuthentication.getRegions()).toArray()[0];
+			List<Container> containers = swiftdb.listContainers(r, recordcount,
+					startkey);
+
+			for (Container container : containers) {
+				HashMap<String, ByteIterator> fieldResult = new HashMap<String, ByteIterator>();
+
+				if (read(null, container.getName(), fields, fieldResult) != 0) {
+					return 1;
+				}
+
+				result.add(fieldResult);
+
+			}
+		} catch (IOException e) {
+			return 1;
+		}
+
+		return 0;
 		// com.mongodb.DB db = null;
 		// try {
 		// db = mongo.getDB(database);
